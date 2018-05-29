fafb7159933aef0e0d9f8d17623abb65cb0ab901
Jo Mingyu <owlsogul@gmail.com>
2018-05-30 08:32:38 +0900

    add IMainfFrame Interface

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -3,14 +3,18 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class MainFrame extends JFrame{
+import view.interfaces.IMainFrame;
+
+public class MainFrame extends JFrame implements IMainFrame{
 
 	private static final long serialVersionUID = 1L;
+	public static final int PANEL_LEFT = 0, PANEL_RIGHT=1;
 	
 	private FilePanel leftPanel, rightPanel;
 	private JButton btnCmp, btnMergeToLeft, btnMergeToRight;
@@ -20,9 +24,12 @@ public class MainFrame extends JFrame{
 		
 	}
 	
+	// ###################################################
+	// PART: inner function part 
+	// ###################################################
 	private void initComponents() {
-		leftPanel = new FilePanel(0);
-		rightPanel = new FilePanel(1);
+		leftPanel = new FilePanel(PANEL_LEFT);
+		rightPanel = new FilePanel(PANEL_RIGHT);
 		
 		btnCmp = new JButton("Compare");
 		btnMergeToLeft = new JButton("Merge To Left");
@@ -49,5 +56,98 @@ public class MainFrame extends JFrame{
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setSize(400, 400);
 		this.setVisible(true);
+	}
+	
+	private FilePanel getFilePanelById(int idFilePanel) {
+		switch(idFilePanel) {
+		case PANEL_LEFT:
+			return leftPanel;
+		case PANEL_RIGHT:
+		default:
+			return null;
+		}
+	}
+	
+	
+	// ###################################################
+	// PART: Interface part 
+	// ###################################################
+	@Override
+	public void setFilePanelName(int idFilePanel, String name) {
+		FilePanel filePanel = getFilePanelById(idFilePanel);
+		if (filePanel != null) filePanel.setFileName(name);
+		
+	}
+
+	@Override
+	public String getFilePanelName(int idFilePanel) {
+		FilePanel filePanel = getFilePanelById(idFilePanel);
+		if (filePanel == null)	return null;
+		else return filePanel.getFileName();
+	}
+
+	@Override
+	public void setFilePanelContents(int idFilePanel, String contents) {
+		FilePanel filePanel = getFilePanelById(idFilePanel);
+		if (filePanel == null) return;
+		filePanel.setText(contents);
+		
+	}
+
+	@Override
+	public String getFilePanelContents(int idFilePanel) {
+		FilePanel filePanel = getFilePanelById(idFilePanel);
+		if (filePanel == null)	return null;
+		else return filePanel.getText();
+	}
+
+	@Override
+	public void setFilePanelEditable(int idFilePanel, boolean editable) {
+		FilePanel filePanel = getFilePanelById(idFilePanel);
+		if (filePanel == null) return;
+		filePanel.setTextEditable(editable);
+		
+	}
+
+	@Override
+	public boolean getFilePanelEditable(int idFilePanel) {
+		FilePanel filePanel = getFilePanelById(idFilePanel);
+		if (filePanel == null)	return false;
+		else return filePanel.getTextEditable();
+	}
+
+	@Override
+	public int getFilePanelLineColor(int idFilePanel, int lineNo) {
+		FilePanel filePanel = getFilePanelById(idFilePanel);
+		if (filePanel == null)	return -1;
+		else return filePanel.getLineColor(lineNo).getColor();
+	}
+
+	@Override
+	public void setFilePanelLineColor(int idFilePanel, int lineNo, int lineColor) {
+		FilePanel filePanel = getFilePanelById(idFilePanel);
+		if (filePanel == null) return;
+		filePanel.setLineColor(lineNo, LineColor.getLineColor(lineColor));
+		
+		
+	}
+
+	@Override
+	public void paintFilePanelLineColor(int idFilePanel) {
+		FilePanel filePanel = getFilePanelById(idFilePanel);
+		if (filePanel == null) return;
+		filePanel.paintLineColor();
+		
+	}
+
+	@Override
+	public void setFilePanelAction(int idFilePanel, ActionListener loadEvent, ActionListener editEvent,
+			ActionListener saveEvent) {
+		FilePanel filePanel = getFilePanelById(idFilePanel);
+		if (filePanel == null) return;
+		filePanel.setLoadAction(loadEvent);
+		filePanel.setEditAction(editEvent);
+		filePanel.setSaveAction(saveEvent);
+		
 	}
 }
---
+++ src/view/interfaces/IMainFrame.java
@@ -0,0 +1,23 @@
+package view.interfaces;
+
+import java.awt.event.ActionListener;
+
+public interface IMainFrame {
+
+	public void setFilePanelName(int idFilePanel, String name);
+	public String getFilePanelName(int idFilePanel);
+	
+	public void setFilePanelContents(int idFilePanel, String contents);
+	public String getFilePanelContents(int idFilePanel);
+	
+	public void setFilePanelEditable(int idFilePanel, boolean editable);
+	public boolean getFilePanelEditable(int idFilePanel);
+
+	public int getFilePanelLineColor(int idFilePanel, int lineNo);
+	public void setFilePanelLineColor(int idFilePanel, int lineNo, int lineColor);
+	public void paintFilePanelLineColor(int idFilePanel);
+	
+	
+	public void setFilePanelAction(int idFilePanel, ActionListener loadEvent, ActionListener editEvent, ActionListener saveEvent);
+
+}
